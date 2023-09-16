c417c6e3748ccb788ee9c96d6990b5c0f2d214b4
Tuan Thanh <tuanthanh.nguyeeen@gmail.com>
2023-09-17 02:32:40 +0700

    fix: rewrite class
    + MUST BE CAREFULL WHEN PASS METHOD AS FIRST CLASS OBJECT
      + because they dont understand the context of this, it only knows the
        plain object

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const express = require('express')
 const finalhandler = require('finalhandler')
 const diContainer = require('./lib/diContainer')
 const TOKEN_SECRET = '712378'
+const dbName = 'class'
 
-diContainer.register('dbName', 'class')
+diContainer.register('dbName', dbName)
 diContainer.register('tokenSecret', TOKEN_SECRET)
 diContainer.factory('db', require('./lib/db'))
 diContainer.factory('authService', require('./lib/authService'))
 diContainer.factory('authController', require('./lib/authController'))
 
 const authController = diContainer.get('authController')
-console.log(diContainer)
 
 const app = express()
 app.use(express.json())
 
 app.get('/ping', (req, res) => { throw new Error('stuff') })
-app.post('/login', authController.login)
-app.post('/check-token', authController.checkToken)
+app.post('/login', authController.login.bind(authController))
+app.post('/check-token', authController.checkToken.bind(authController))
 
 // return 404 if not found
 app.all(() => finalhandler())
@@ -26,7 +26,7 @@ app.all(() => finalhandler())
 app.use((error, req, res, next) => {
   let done = finalhandler(req, res, { 
       // env: 'production',
-      onerror: err => { console.error(err.message) }
+      onerror: err => { console.error(err) }
     }
   )
 
